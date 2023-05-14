58a6f8c777ac5b0f40bbf98351d8006d86f01156
mbs3523 <130825370+mbs3523@users.noreply.github.com>
2023-05-14 18:38:23 +0300

    code

---
+++ main.py
@@ -0,0 +1,15 @@
+def f_dict(dicts,d={},keys=''):
+    if isinstance(dicts,dict):
+        return 'Not dict'
+    for key in dicts:
+        if isinstance(dicts.get(key),dict):
+            if keys == '':
+                keys = f'{key}' + f'.{keys}'
+            else:
+                keys = keys + f'{key}.'
+            f_dict(dicts.get(key),d,keys)
+            if  len(keys) == 2:
+                keys=''
+        else:
+            d.setdefault(f'{keys}'+key,dicts.get(key))
+    return d
